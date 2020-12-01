5df8010ac03522afed1a0a6e94439eeca4f210d3
liflonyan <lisett961@hotmail.com>
2020-12-01 20:21:14 -0300

    Creacion Pagina de Contacto con formulario  de la tabla consulta

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SistemaVentas.Models;
 
 namespace SistemaVentas.Controllers
 {
     public class HomeController : Controller
     {
+
+        private AguilaCurdaEntities db = new AguilaCurdaEntities();
         public ActionResult Index()
         {
             return View();
@@ -20,18 +23,33 @@ namespace SistemaVentas.Controllers
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
         public ActionResult Nosotros()
         {
-            ViewBag.Message = "Your application description page.";
 
             return View();
         }
+
+        public ActionResult Contacto()
+        {
+            return View();
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Contacto([Bind(Include = "id_consulta,nombre,email,comentario")] Consulta consulta)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Consulta.Add(consulta);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(consulta);
+        }
+
+        
 
     }
 }
